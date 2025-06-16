376283b3620678f4499962c7995f8a070cbf58b4
ConfiG <cgytrus@cgyt.ru>
2025-06-16 17:19:13 +0300

    Fuck

--- src/custom-percent-decimal.cpp
+++ src/custom-percent-decimal.cpp
@@ -10,15 +10,15 @@ $execute {
     constexpr ptrdiff_t addr =
 #ifdef GEODE_IS_WINDOWS
         0x5ad850;
-#elif GEODE_IS_ANDROID32
+#elifdef GEODE_IS_ANDROID32
         0x932e13;
-#elif GEODE_IS_ANDROID64
+#elifdef GEODE_IS_ANDROID64
         0xe79c08;
-#elif GEODE_IS_ARM_MAC
+#elifdef GEODE_IS_ARM_MAC
         0x7350cc;
-#elif GEODE_IS_INTEL_MAC
+#elifdef GEODE_IS_INTEL_MAC
         0x811d82;
-#elif GEODE_IS_IOS
+#elifdef GEODE_IS_IOS
         0x6c2dd9;
 #else
         0x0;
