5a7db89d293f2d6a70e0d0522dcf5dec08d44b81
ConfiG <cgytrus@cgyt.ru>
2025-06-17 10:23:43 +0300

    robot/spider extra sprite parenting fix

---
+++ src/portable/robot-spider-extra-parenting-fix.cpp
@@ -0,0 +1,17 @@
+﻿#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/GJRobotSprite.hpp>
+#include <utility>
+class $modify(GJRobotSprite) {
+    // TODO: move to ctor when modify with it is fixed in geode
+    $override bool init(int p0, std::string p1) {
+        // m_headSprite not being initialized sometimes causes a check in init to pass
+        // causing the extra sprite to be attached to whatever was in m_headSprite
+        // instead of the actual head sprite
+        if (!Mod::get()->getSettingValue<bool>("robot-spider-extra-parenting-fix"))
+            m_headSprite = nullptr;
+        return GJRobotSprite::init(p0, std::move(p1));
+    }
+};
