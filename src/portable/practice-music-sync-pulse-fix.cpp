31ed0dd1ff16f50da01be93b0dd33a903dc3d146
ConfiG <cgytrus@cgyt.ru>
2025-06-17 16:42:15 +0300

    practice music sync pulse fix

---
+++ src/portable/practice-music-sync-pulse-fix.cpp
@@ -0,0 +1,18 @@
+﻿#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/AudioEffectsLayer.hpp>
+class $modify(AudioEffectsLayer) {
+    $override void audioStep(float p0) {
+        if (!Mod::get()->getSettingValue<bool>("practice-music-sync-pulse-fix"))
+            return AudioEffectsLayer::audioStep(p0);
+        auto* pl = PlayLayer::get();
+        if (!pl)
+            return AudioEffectsLayer::audioStep(p0);
+        bool saved = pl->m_isPracticeMode;
+        pl->m_isPracticeMode = pl->m_isPracticeMode && !pl->m_practiceMusicSync;
+        AudioEffectsLayer::audioStep(p0);
+        pl->m_isPracticeMode = saved;
+    }
+};
