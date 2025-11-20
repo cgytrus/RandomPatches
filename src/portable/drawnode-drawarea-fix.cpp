51c9c3868ed44f2c3eb210d73be191ed389478a6
ConfiG <cgytrus@cgyt.ru>
2025-11-21 01:53:56 +0300

    ccdrawnode draw area fix

---
+++ src/portable/drawnode-drawarea-fix.cpp
@@ -0,0 +1,11 @@
+﻿#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/CCDrawNode.hpp>
+class $modify(CCDrawNode) {
+    $override bool init() {
+        m_bUseArea = false;
+        return CCDrawNode::init();
+    }
+};
